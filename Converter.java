dc00b79974d120c26bd7a2f7325b2fbb1b73f8fd
Bibin Graceson J <118437373+bibinwithab@users.noreply.github.com>
2023-08-15 22:43:06 +0530

    Added initial files

---
+++ Converter.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Converter {
+    
+    public void converter(){
+
+        
+
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main extends Converter{
+    public static void main(String[] args) {
+        Converter start = new Converter();
+        start.converter();
+        return;
+    }
+}
