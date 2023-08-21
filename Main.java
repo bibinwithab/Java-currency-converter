0d23ddd9a21bb74ec1c85d8400ebd3e2788295cf
bibinwithab <bibingraceson@gmail.com>
2023-08-21 16:26:32 +0530

    added icon & removed converter class

--- Main.java
+++ Main.java
@@ -1,7 +1,78 @@
-public class Main extends Converter{
+import java.text.DecimalFormat;
+
+import java.awt.Color;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class Main{
     public static void main(String[] args) {
-        Converter start = new Converter();
-        start.convert();
-        return;
+        DecimalFormat df = new DecimalFormat("#.###");
+        JFrame frame = new JFrame("Currency Converter");
+
+        JLabel l1,l2;
+        JTextField t1, t2;
+        JButton b1,b2;
+
+        l1 = new JLabel("Rupee: ");
+        l1.setBounds(20,40,60,30);
+        t1 = new JTextField(20);
+        t1.setBounds(100,40,100,30);
+
+        l2 = new JLabel("Dollar: ");
+        l2.setBounds(20,80,60,30);
+        t2 = new JTextField(20);
+        t2.setBounds(100,80,100,30);
+
+        b1 = new JButton("INR to USD");
+        b1.setBounds(20,150,130,30);
+
+        b2 = new JButton("USD to INR");
+        b2.setBounds(160,150,130,30);
+
+
+        b1.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                String s1 = t1.getText();
+                double a = Double.parseDouble(s1);
+                double b = a/83.19;
+                String s2 = String.valueOf(df.format(b));
+                t2.setText(s2);
+            }
+        });
+
+        b2.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                String s3 = t2.getText();
+                double c = Double.parseDouble(s3);
+                double d = c*83.19;
+                String s4 = String.valueOf(df.format(d));
+                t1.setText(s4);
+            }
+        });
+
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                frame.dispose();
+            }
+        });
+
+        
+        frame.add(l1);
+        frame.add(l2);
+        frame.add(t1);
+        frame.add(t2);
+        frame.add(b1);
+        frame.add(b2);
+
+        ImageIcon icon  = new ImageIcon("img/icon/jpeg");
+
+        frame.setIconImage(icon.getImage());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(350,250);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        frame.getContentPane().setBackground(new Color(222, 13, 342, 0));
+
     }
 }
